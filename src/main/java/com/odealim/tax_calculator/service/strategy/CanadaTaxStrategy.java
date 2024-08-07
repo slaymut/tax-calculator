f4e8eecff3eeb4d5ac5b723a30bc0165396c2984
Slaymut <slimani.salim.pro@gmail.com>
2024-08-07 10:39:24 +0200

    Forgot to register canada and france as components

--- src/main/java/com/odealim/tax_calculator/service/strategy/CanadaTaxStrategy.java
+++ src/main/java/com/odealim/tax_calculator/service/strategy/CanadaTaxStrategy.java
@@ -2,8 +2,11 @@ package com.odealim.tax_calculator.service.strategy;
 
 import java.math.BigDecimal;
 
+import org.springframework.stereotype.Component;
+
 import com.odealim.tax_calculator.model.Product;
 
+@Component("canada")
 public class CanadaTaxStrategy implements TaxStrategy {
   private static final BigDecimal CANADA_TAX_RATE = new BigDecimal(0.05);
 
--- src/main/java/com/odealim/tax_calculator/service/strategy/FranceTaxStrategy.java
+++ src/main/java/com/odealim/tax_calculator/service/strategy/FranceTaxStrategy.java
@@ -2,8 +2,11 @@ package com.odealim.tax_calculator.service.strategy;
 
 import java.math.BigDecimal;
 
+import org.springframework.stereotype.Component;
+
 import com.odealim.tax_calculator.model.Product;
 
+@Component("france")
 public class FranceTaxStrategy implements TaxStrategy {
   private static final BigDecimal FRANCE_TAX_RATE = new BigDecimal(0.20);
 
